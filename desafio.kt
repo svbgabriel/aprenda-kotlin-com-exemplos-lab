746d83f0c2879584a36908e2f98fd30fd29695c5
Gabriel Batista <svbgabriel@gmail.com>
2023-11-26 17:37:08 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,63 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String, val idade: Int, val cpf: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
 
     val inscritos = mutableListOf<Usuario>()
     
+    fun adicionarConteudo(conteudo: ConteudoEducacional) = conteudos.add(conteudo)
+    fun removerConteudo(conteudo: ConteudoEducacional) = conteudos.remove(conteudo)
+    
+    fun printConteudos() = println("Conteudos da formação: $conteudos")
+    
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (inscritos.contains(usuario)) {
+            println("Usuário ${usuario.nome} já está matriculado na formação $nome!")
+            return;
+        }
+        
+        inscritos.add(usuario);
+        println("Usuário ${usuario.nome} foi matriculado na formação $nome com sucesso!")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João", 23, "11111111111")
+    val usuario2 = Usuario("Pedro", 32, "11111111112")
+    val usuario3 = Usuario("Maria", 40, "11111111113")
+    
+    val formacao1 = Formacao("Iniciando com Kotlin")
+    val formacao2 = Formacao("Rust Avançado")
+    val formacao3 = Formacao("Iniciando rápido com Go")
+    
+    val conteudoKotlin1 = ConteudoEducacional("Variáveis em Kotlin", 10)
+    val conteudoKotlin2 = ConteudoEducacional("Funções em Kotlin", 20)
+    val conteudoRust1 = ConteudoEducacional("O que é Ownership", 20)
+    val conteudoGo1 = ConteudoEducacional("Variáveis em Go", 5)
+    
+    // Adiciona os conteúdos para a formação de Kotlin
+    formacao1.adicionarConteudo(conteudoKotlin1)
+    formacao1.adicionarConteudo(conteudoKotlin2)
+    formacao1.adicionarConteudo(conteudoRust1)
+    formacao1.printConteudos()
+    // Ops um conteúdo errado foi adicionado
+    formacao1.removerConteudo(conteudoRust1)
+    // Corrigido!
+    formacao1.printConteudos()
+    formacao2.adicionarConteudo(conteudoRust1)
+    formacao2.printConteudos()
+    formacao3.adicionarConteudo(conteudoGo1)
+    formacao3.printConteudos()
+    
+    // Matricula os alunos
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario1)
+    formacao3.matricular(usuario2)
+    formacao2.matricular(usuario3)
+    formacao2.matricular(usuario2)
 }
